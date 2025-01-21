153e3ef2435c24cf22aa1ce9672f0a81a7cc2c1f
Aryan sharma <aryansharma99107@gmail.com>
2025-01-21 18:49:45 +0530

    added feat one

--- server/Middlewares/auth.js
+++ server/Middlewares/auth.js
@@ -8,7 +8,7 @@ exports.auth = async (req , res , next) => {
     try{
        
        const token = req.body.token || req.cookies.token  || req.header("Authorization")?.replace("Bearer ", ""); //and possibly other wats too header se nikl laao 
-
+       console.log(token);
        if(!token){
           return res.status(401).json({
              success:false , 
